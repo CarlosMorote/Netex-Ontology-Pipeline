53a7459cb1fc09783d9e6bf4ecc83585925e60dd
CarlosMorote <Carlos.Morote1@gmail.com>
2022-06-17 18:46:53 +0200

    Turtle Prefixes

--- src/main/java/DataManager/Ontology/RDFManager.java
+++ src/main/java/DataManager/Ontology/RDFManager.java
@@ -6,13 +6,14 @@ import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.RDF;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RDFManager {
     private String ont_source;
@@ -28,14 +29,33 @@ public class RDFManager {
     public RDFManager(String ont_source) {
         this.ont_source = ont_source;
         this.ont = this.loadModel(ont_source);
-        this.rdf = ModelFactory.createDefaultModel();
+        this.init_rdf(null);
     }
 
     public RDFManager(String ont_source, String ont_dir_output) {
         this.ont_dir_output = ont_dir_output;
         this.ont_source = ont_source;
         this.ont = this.loadModel(ont_source);
+        this.init_rdf(null);
+    }
+
+    public void init_rdf(Map<String, String> map){
         this.rdf = ModelFactory.createDefaultModel();
+        if (map == null){
+            map = new HashMap<>(){{
+                put("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+                put("schema", "https://schema.org/");
+                put("journeys", Namespaces.JOURNEYS +"#");
+                put("vcard", "http://www.w3.org/2006/vcard/ns#");
+                put("skos", "http://www.w3.org/2004/02/skos/core#");
+                put("transmodel", Namespaces.TRANSMODEL_ROOT);
+                put("organisations", Namespaces.ORGANISATIONS+"#");
+                put("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+                put("commons", Namespaces.COMMONS+"#");
+                put("facilities", Namespaces.FACILITIES+"#");
+            }};
+        }
+        this.rdf.setNsPrefixes(map);
     }
 
     public OntModel loadModel(String source){
@@ -68,11 +88,10 @@ public class RDFManager {
     }
 
     public void saveRDF(String output, String format){
-        FileWriter out = null;
+        OutputStream out;
         try {
-            out = new FileWriter(output);
+            out = new FileOutputStream(output);
             this.rdf.write(out, format);
-            out.close();
             System.out.println("LinkedData generated. File in "+output);
         } catch (IOException e) {
             throw new RuntimeException(e);
